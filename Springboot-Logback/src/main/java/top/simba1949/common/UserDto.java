3ea1435c49100ec427105876d185d158f90ceb67
simba <simba@jarviss.top>
2018-09-09 13:46:46 +0800

    Springboot集成Logback

---
+++ Springboot-Logback/src/main/java/top/simba1949/common/UserDto.java
@@ -0,0 +1,15 @@
+package top.simba1949.common;
+
+import lombok.Data;
+
+/**
+ * @author simba1949@outlook.com
+ * @date 2018/9/9 13:42
+ */
+@Data
+public class UserDto {
+
+    private Long id;
+    private String username;
+    private String password;
+}
