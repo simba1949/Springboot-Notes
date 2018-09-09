fbf9a608dad16f790d18d48470099815e745125d
simba <simba@jarviss.top>
2018-09-09 13:32:24 +0800

    Springboot测试集成Swagger2

---
+++ Springboot-Swagger2/doc/Springboot集成Swagger2.md
@@ -0,0 +1,3 @@
+# Springboot集成Swagger2
+
+https://blog.csdn.net/SIMBA1949/article/details/80919183
---
+++ Springboot-Swagger2/src/main/java/top/simba1949/common/UserDto.java
@@ -0,0 +1,14 @@
+package top.simba1949.common;
+
+import lombok.Data;
+
+/**
+ * @author simba1949@outlook.com
+ * @date 2018/9/9 9:19
+ */
+@Data
+public class UserDto {
+    private Long id;
+    private String username;
+    private String password;
+}
